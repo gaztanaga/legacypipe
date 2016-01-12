4c314215eda38b830f0a7bdbad98994319d3a6dd
Kaylan Burleigh <kburleigh@lbl.gov>
2016-01-12 12:25:42 -0800

    dustin recommended invvar as var_image instead of invvar_e, so photo-electrons, not image DN, is poisson distributed: var_e= Ne, Ne= gain*DN, so var_DN= gain/DN. ....implemented in legacypipe/runptf.py

--- py/legacypipe/runptf.py
+++ py/legacypipe/runptf.py
@@ -160,7 +160,7 @@ class PtfImage(LegacySurveyImage):
         '''return gain*pixel DN as numpy array'''
         print('Reading image from', self.imgfn, 'hdu', self.hdu)
         img,hdr= fitsio.read(self.imgfn, ext=self.hdu, header=True) 
-        return hdr['GAIN']*img,hdr 
+        return img,hdr 
 
     def read_dq(self,**kwargs):
         '''return bit mask which Tractor calls "data quality" image
@@ -193,7 +193,7 @@ class PtfImage(LegacySurveyImage):
         img,hdr=self.read_image(header=True)
         assert(dq.shape == img.shape)
         invvar=np.zeros(img.shape)
-        invvar[dq == 0]= np.power(img[dq == 0], -1) #img is already gain*pixel dn
+        invvar[dq == 0]= hdr['GAIN']/img[dq == 0] #img is already gain*pixel dn
         #invvar[dq == 2]= np.power(img[dq == 2],-0.5) #mask-2 already done, bit 2^1 for SExtractor ojbects
         if clip:
             # Clamp near-zero (incl negative!) invvars to zero.
@@ -612,6 +612,8 @@ def main():
     kwargs['bands'] = 'gR'
 
     # runbrick...
+    print("before call run_brick")
+    print("pixscale= ",opt.pixscale)
     run_brick(opt.brick, **kwargs)
     return 0
     
