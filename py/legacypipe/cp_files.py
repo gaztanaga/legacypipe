6e301d35536376b762e635402e65c89b76e48422
Kaylan Burleigh <kburleigh@lbl.gov>
2016-02-03 13:56:57 -0800

    grabs 'N' PTF images filtered by band and ptf field

--- py/legacypipe/cp_files.py
+++ py/legacypipe/cp_files.py
@@ -1,25 +1,27 @@
 import os
 import argparse
 import glob
-import fitsio
+from astropy.io import fits
 import numpy as np
 
 parser = argparse.ArgumentParser(description="test")
 parser.add_argument("-Ncopy",type=int,action="store",help='# of images to copy from /project to /scratch')
 parser.add_argument("-band",choices=['g','R'],action="store",help='which band')
-parser.add_argument("-image_dir",action="store",help='/project processed image dir')
+parser.add_argument("-ptf_field",choices=['120001', '002740', '002845'],action="store",help='(RA,DEC)=(148.93, 1.651), (149.142857, 1.125),(149.142857, 3.375)')
 parser.add_argument("-outdir",action="store",help='directory to make pimage/ and mask/ in for storing the copied images')
 args = parser.parse_args()
 
 for mydir in ['images']:
     if not os.path.exists(os.path.join(args.outdir,mydir)): os.makedirs(os.path.join(args.outdir,mydir))
 
-search_str= '*_scie_*_f0%d_*.fits' % 2
-if args.band == 'g': search_str= '*_scie_*_f0%d_*.fits' % 1
+key=dict(g='_f01_',R='_f02_')
 copied=0
-for fn in glob.glob(os.path.join(args.image_dir,search_str)):
-    hdr=fitsio.read_header(fn,ext=0)
-    if type(hdr['IMAGEZPT']) == np.float:    
+ptf_dir='/global/project/projectdirs/desi/imaging/data/ptf/cosmos/seeing_lt_3_airmass_lt_2'
+fns= glob.glob(os.path.join(ptf_dir, 'PTF_*_scie_*%s*p%s*.fits' %(key[args.band],args.ptf_field) ))
+if len(fns) == 0: print 'WARNING, 0 files found'
+for fn in fns:
+    data=fits.open(fn)
+    if type(data[0].header['IMAGEZPT']) == np.float:    
         copied +=1
         cmd= ' '.join(['cp',fn,os.path.join(args.outdir,'images/',os.path.basename(fn))])
         if os.system(cmd): raise ValueError
